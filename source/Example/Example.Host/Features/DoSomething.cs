3b4c28f6df780e1938eabedd7eb6c751a87720e7
Sebastian Schwarz <Sebastian.Schwarz@generic.de>
2018-07-23 22:37:44 +0200

    added example feature

---
+++ source/Example/Example.Host/Features/DoSomething.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Example.Contracts.Repositories;
+
+namespace Example.Host.Features
+{
+    public class DoSomething
+    {
+        private readonly IExampleRepository _exampleRepository;
+
+        public DoSomething(IExampleRepository exampleRepository)
+        {
+            _exampleRepository = exampleRepository;
+        }
+
+        public int Execute(string param)
+        {
+            return 1;
+        }
+    }
+}
---
+++ source/Example/Example.Repositories/ExampleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Repositories
+{
+    public class ExampleRepository
+    {
+        private readonly string _connectionString;
+
+        public ExampleRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+}
