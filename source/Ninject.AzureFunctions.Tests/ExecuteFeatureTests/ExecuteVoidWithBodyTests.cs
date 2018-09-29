9a123b5c787e7dcd32ff74eedc8d41d5859c15ab
Sebastian Schwarz <Sebastian.Schwarz@generic.de>
2018-09-29 12:27:23 +0200

    add ExecuteVoidWithBody

---
+++ source/Ninject.AzureFunctions.Tests/ExecuteFeatureTests/ExecuteVoidWithBodyTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ninject.AzureFunctions.Contracts;
+using Ninject.AzureFunctions.Features;
+using Ninject.AzureFunctions.Tests.Utility;
+using NUnit.Framework;
+
+namespace Ninject.AzureFunctions.Tests.ExecuteFeatureTests
+{
+    public class ExecuteVoidWithBodyTests
+    {
+        [Test]
+        public async Task ExecutesVoidWithBodyFeature()
+        {
+            var kernelContainer = new FakeKernelContainer();
+            var fakeService = kernelContainer.Kernel.Get<IFakeService>();
+            var request = new FakeHttpRequest<string>("Test");
+
+            var result =
+                await ExecuteFeature.ExecuteVoidWithBody<VoidWithBodyFeature,string>(kernelContainer, request,(f,b) => f.Execute(b)) as
+                    OkResult;
+
+            Assert.That(fakeService.Value, Is.EqualTo("Test"));
+            Assert.That(result, Is.Not.Null);
+
+            request.Dispose();
+        }
+
+        [Test]
+        public async Task ExecutesVoidWithBodyFeatureThrowsException()
+        {
+            var kernelContainer = new FakeKernelContainer();
+            var request = new FakeHttpRequest<string>("Test");
+
+            var result =
+                await ExecuteFeature.ExecuteVoidWithBody<VoidWithBodyFeatureWithException,string>(kernelContainer, request,(f, b) => f.Execute(b));
+
+            Assert.That(result.GetType(), Is.EqualTo(typeof(InternalServerErrorResult)));
+            request.Dispose();
+        }
+
+
+
+        internal class VoidWithBodyFeature : IFeature
+        {
+            private readonly IFakeService _fakeService;
+
+            public VoidWithBodyFeature(IFakeService fakeService)
+            {
+                _fakeService = fakeService;
+            }
+
+            public async Task<IActionResult> Execute(string value)
+            {
+                _fakeService.SetValue(value);
+                return await Task.FromResult(new OkObjectResult("Bla"));
+            }
+        }
+
+        internal class VoidWithBodyFeatureWithException : IFeature
+        {
+            private readonly IFakeService _fakeService;
+
+            public VoidWithBodyFeatureWithException(IFakeService fakeService)
+            {
+                _fakeService = fakeService;
+            }
+
+            public async Task<IActionResult> Execute(string value)
+            {
+                await Task.Run(() => throw new ArgumentException());
+                return new OkObjectResult("Bla");
+            }
+        }
+    }
+}
---
+++ source/Ninject.AzureFunctions.Tests/Utility/FakeHttpRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Ninject.AzureFunctions.Tests.Utility
+{
+    public class FakeHttpRequest<T> : HttpRequest,IDisposable
+    {
+        public FakeHttpRequest(T body)
+        {
+            Body = new MemoryStream();
+            Writer = new StreamWriter(Body);
+
+            var serializedBody = JsonConvert.SerializeObject(body);
+            Writer.WriteLine(serializedBody);
+            Writer.Flush();
+            Body.Seek(0, SeekOrigin.Begin);
+            
+
+        }
+
+        public StreamWriter Writer { get; set; }
+
+        public override async Task<IFormCollection> ReadFormAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public override HttpContext HttpContext { get; }
+        public override string Method { get; set; }
+        public override string Scheme { get; set; }
+        public override bool IsHttps { get; set; }
+        public override HostString Host { get; set; }
+        public override PathString PathBase { get; set; }
+        public override PathString Path { get; set; }
+        public override QueryString QueryString { get; set; }
+        public override IQueryCollection Query { get; set; }
+        public override string Protocol { get; set; }
+        public override IHeaderDictionary Headers { get; }
+        public override IRequestCookieCollection Cookies { get; set; }
+        public override long? ContentLength { get; set; }
+        public override string ContentType { get; set; }
+        public override Stream Body { get; set; }
+        public override bool HasFormContentType { get; }
+        public override IFormCollection Form { get; set; }
+
+        public void Dispose()
+        {
+            Body?.Dispose();
+            Writer?.Dispose();
+        }
+    }
+}
